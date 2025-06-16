d980ece83e34acfebfc368e459cff3d4e045371e
Marcelo Moreira <marcelochave059@gmail.com>
2025-06-15 22:54:40 -0300

    migrations

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+
+
 def index (request):
-    return render(request,'galeria/index.html' )
+    dados = {
+    1:{"nome":"Ganbatte", "legenda":"Nobuo Suzuki / 2022 / Sextante"},
+    2:{"nome":"Essencialismo", "legenda":"Greg McKeown / 2015 / Sextante"}
+}
+    return render(request,'galeria/index.html', {"cards":dados})
 
 def imagem (request):
     return render(request, 'galeria/imagem.html')
