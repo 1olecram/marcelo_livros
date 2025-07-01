acf9debe0e3f83a198c728fc0929e93cb2a7d2ab
Marcelo Moreira <marcelochave059@gmail.com>
2025-07-01 00:01:40 -0300

    Start media use

--- galeria/models.py
+++ galeria/models.py
@@ -14,9 +14,9 @@ class Livro(models.Model):
     legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=opcoes_categoria,default='')
     descricao = models.TextField(null=False,blank=False)
-    foto =  models.CharField(max_length=100, null=False, blank=False)
+    foto =  models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     publicado = models.BooleanField(default=False)
     data_adicao_livro = models.DateTimeField(default=date.now, blank=False)
 
     def __str__(self):
-        return f"Livro [titulo={self.titulo}]"
+        return self.titulo
