c59c35703a2a587a636366a439ebc44fc16b03f6
Marcelo Moreira <marcelochave059@gmail.com>
2025-07-09 09:47:54 -0300

    Mudança footer e atualização geral

--- galeria/views.py
+++ galeria/views.py
@@ -8,3 +8,13 @@ def index (request):
 def imagem (request,foto_id):
     livro = get_object_or_404(Livro, pk = foto_id)
     return render(request, 'galeria/imagem.html',{"livro":livro})
+
+def buscar(request):
+    livros = Livro.objects.order_by("data_adicao_livro").filter(publicado=True)
+
+    if("buscar" in request.GET):
+        titulo_a_buscar = request.GET['buscar']
+        if(titulo_a_buscar):
+            livros = livros.filter(titulo__icontains = titulo_a_buscar)
+
+    return render (request, "galeria/buscar.html",{"cards":livros})
